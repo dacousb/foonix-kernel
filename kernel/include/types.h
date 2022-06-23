cd95d646fd07d9ae07c46b7a4ea52b5b410feec5
dacousb <usbdaco@protonmail.com>
2022-06-23 20:26:01 +0200

    Use fixed size types

--- kernel/include/types.h
+++ kernel/include/types.h
@@ -1,16 +1,18 @@
 #ifndef __TYPES_H__
 #define __TYPES_H__
 
+#include <stdint.h>
+
 #define nil ((void *)0)
 
-typedef unsigned char u8;
-typedef short unsigned int u16;
-typedef unsigned int u32;
-typedef long unsigned int u64;
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
 
-typedef signed char i8;
-typedef short int i16;
-typedef int i32;
-typedef long int i64;
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
 
 #endif
